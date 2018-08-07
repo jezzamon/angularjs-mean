1e8c0d5ade2aef3c0f30faa8747b5b272efb05fd
jezzamondev <jezzamondev@gmail.com>
2018-08-06 21:11:53 -0400

    adding stylus, templates

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'pug');
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+
+// any requests that much files in public directory , return the file
+app.use(express.static(__dirname + '/public'));  
+
 
 app.get('*', function(req, res) {
   res.render('index');
