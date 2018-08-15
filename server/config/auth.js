0b635b5c0794e83aabb26ef4ea3632f13f6f5e7f
jezzamondev <jezzamondev@gmail.com>
2018-08-15 06:43:26 -0400

    AUTHORIZATION - implementing server side authorization to check is user is admin

--- server/config/auth.js
+++ server/config/auth.js
@@ -9,6 +9,10 @@ module.exports.authenticate = function(req, res, next) {
     if(!user) { res.send({success: false})}
 
     //login function added by passport to req obj
+    // note: Typically with passport we log in with server side route (submitted form)
+    // but here we are logging in using XHR post, normally you dont have to tell
+    // passport to login the user itself but since we are using XHR we need  req.logIn 
+
     req.logIn(user, function(err) {
       if(err) { return next(err);}
       
@@ -17,10 +21,27 @@ module.exports.authenticate = function(req, res, next) {
       );
     });
   });
-
   auth(req, res, next);
 }
 
-// note: Typically with passport we log in with server side route (submitted form)
-// but here we are logging in using XHR post, normally you dont have to tell
-// passport to login the user itself but since we are using XHR we need  req.logIn 
+exports.requiresApiLogin = function(req, res, next) {
+  // isAuthenticated is a method passed in by passport to req object
+  if(!req.isAuthenticated()) {
+    res.status(403) // 403 - forbidden
+    res.end();
+  } else {
+    next();
+  }
+}
+
+exports.requiresRole = function(role) {
+  return function(req, res, next) {
+    if(!req.isAuthenticated() || req.user.roles.indexOf(role) === -1) {
+      res.status(403);
+      res.end();
+    } else {
+      next();
+    }
+  }
+}
+
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,19 @@
 var auth = require('./auth');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 module.exports = function(app) {
-    // app.get('/partials/:partialPath', function(req, res) {
+
+  app.get('/api/users', auth.requiresRole('admin'), function(req,res) {
+    User.find({}).exec(function(err, collection) {
+      res.send(collection);
+    })
+  });
+
+  // app.get('/partials/:partialPath', function(req, res) {
   //   res.render('partials/' + req.params.partialPath);
   // });
 
-  // IMPORTANT: this route must come before app.get('*') routing to avoid circular errors
   app.get('/partials/*', function(req, res) {
     console.log(req.params[0])
     res.render('../../public/app/' + req.params[0]);  // the first index will be what matches with asterisk
@@ -19,6 +27,7 @@ module.exports = function(app) {
     res.end();
   });
 
+   // IMPORTANT: this route must come last  - app.get('*') routing to avoid circular errors
   app.get('*', function(req, res) {
     res.render('index', {
       bootstrappedUser: req.user
