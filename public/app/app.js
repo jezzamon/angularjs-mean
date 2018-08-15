6c3226efa487357a061302b3ad2c439df344da95
Jerry Relunia <jrelunia@bhvr.com>
2018-08-15 08:50:58 -0400

    SECURITY AND AUTHORIZATION - protect admin pages on client

--- public/app/app.js
+++ public/app/app.js
@@ -3,5 +3,28 @@ angular.module('app', ['ngResource', 'ngRoute'])
   .config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
-    $routeProvider.when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl' });
+    $routeProvider
+      .when('/', { templateUrl: '/partials/main/main', controller: 'mvMainCtrl' })
+      .when('/admin/users', { templateUrl: '/partials/admin/user-list', 
+        controller: 'mvUserListCtrl',
+        resolve: {
+          auth: function(mvIdentity, $q) {
+            if (mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1) {
+              return true;
+            } else {
+              return $q.reject('not authorized')
+            }
+          }
+        }
+      });
+      
   });
+
+  // runs after module has loaded and completed its stack
+  angular.module('app').run(function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+      if(rejection === 'not authorized') {
+        $location.path('/');
+      }
+    })
+  })
