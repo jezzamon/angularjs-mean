7e4f4fbdb212c4fe8022d19ede42c7b6c7c0e467
jezzamondev <jezzamondev@gmail.com>
2018-08-16 19:48:59 -0400

    IMPLEMENTING SIGNUP - refactor server side mongoose Model

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var encrypt = require('../utilities/encryption');
+var userModel = require('../models/User');
 
 module.exports = function (config) {
   // mongoose connection
@@ -11,38 +11,6 @@ module.exports = function (config) {
     console.log('your db is connected');
   });
 
-  var userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    salt: String,
-    hashed_pwd: String,
-    roles: [String]
-  });
-
-  userSchema.methods = {
-    authenticate: function(passwordToMatch) {
-      return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-    }
-  }
-
-  var User = mongoose.model('User', userSchema);
-
-  User.find({}).exec(function(err, collection) {
-    if(collection.length === 0 ) {
-      var salt, hash;
-      salt = encrypt.createSalt();
-      hash = encrypt.hashPwd(salt, 'joe')
-
-      User.create({ firstName: 'Jerry', lastName: 'Relunia', 
-        username: 'jez', salt: salt, hashed_pwd: hash,
-        roles: ['admin']});
-      User.create({ firstName: 'Jessica', lastName: 'Gaulin', 
-        username: 'jessie', salt: salt, hashed_pwd: hash,
-        roles: []});
-        User.create({ firstName: 'Clark', lastName: 'Kent', 
-        username: 'jessie', salt: salt, hashed_pwd: hash});
-    }
-  });
+  userModel.createDefaultUsers();
 
 }
