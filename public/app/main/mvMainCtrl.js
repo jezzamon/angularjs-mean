32fa477cf3b052cb6fec87e3035606db66846bd0
jezzamondev <jezzamondev@gmail.com>
2018-08-18 12:30:10 -0400

    refactor to load courses from db

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,21 +1,23 @@
 angular.module('app')
     .controller('mvMainCtrl', [
-    '$scope', 
-    function($scope) {
+    '$scope',
+    'mvShows',
+    function($scope, mvShows) {
 
 
-    $scope.shows = [
-      {title: 'Terrace House', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Serie Noir', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Outlander', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Better Call Saul', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Fargo', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Breaking Bad', featured: true, published: new Date(2018, 3, 16, 33)},
-      {title: 'Outlander', featured: false, published: new Date(2018, 3, 16, 33)},
-      {title: 'Barry', featured: false, published: new Date(2018, 3, 16, 33)},
-      {title: 'Insecure', featured: false, published: new Date(2018, 3, 16, 33)},
-      {title: 'Atlanta', featured: false, published: new Date(2018, 3, 16, 33)},
-      {title: 'The defiant ones', featured: false, published: new Date(2018, 3, 16, 33)},
-      
-    ]
+    $scope.shows = mvShows.query();
 }]);
+
+
+
+// {title: 'Terrace House', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Serie Noir', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Outlander', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Better Call Saul', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Fargo', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Breaking Bad', featured: true, published: new Date(2018, 3, 16, 33)},
+// {title: 'Outlander', featured: false, published: new Date(2018, 3, 16, 33)},
+// {title: 'Barry', featured: false, published: new Date(2018, 3, 16, 33)},
+// {title: 'Insecure', featured: false, published: new Date(2018, 3, 16, 33)},
+// {title: 'Atlanta', featured: false, published: new Date(2018, 3, 16, 33)},
+// {title: 'The defiant ones', featured: false, published: new Date(2018, 3, 16, 33)},
