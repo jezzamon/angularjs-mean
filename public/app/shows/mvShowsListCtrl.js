d4448e6c1a55dab8a64917dcbeb329ed4174a41a
jezzamondev <jezzamondev@gmail.com>
2018-08-18 11:56:08 -0400

    SHOWS LIST - filter and sorting

--- public/app/shows/mvShowsListCtrl.js
+++ public/app/shows/mvShowsListCtrl.js
@@ -3,5 +3,13 @@ angular.module('app')
     '$scope',
     'mvShows',
     function($scope, mvShows) {
+
       $scope.shows = mvShows.query();
+
+      $scope.sortOptions = [
+        {value: "title", text: "Sort by Title"},
+        {value: "published", text: "Sort by Published Date"},
+      ]
+
+      $scope.sortOrder = $scope.sortOptions[0].value;
   }]);
